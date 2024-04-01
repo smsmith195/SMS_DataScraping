a774e2e7fb420b86df2be0c716e167ed6627229c
Stephen Mark Smith <smsmith195@gmail.com>
2024-04-01 20:06:22 +0100

    Update indeedScrape.py to include company names and locations in scrape data

--- app/indeedScrape.py
+++ app/indeedScrape.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 
 # User input variables for searching a job title and location
@@ -10,18 +9,20 @@ jobLocation = input("Where are you looking for work? ")
 # Launch Firefox on the Indeed homepage
 driver = webdriver.Firefox()
 driver.get("https://uk.indeed.com/")
-driver.implicitly_wait(5)
 
 # Variables for search fields and submit button
 jobSearchField = "text-input-what"
 jobLocationField = "text-input-where"
 submitButton = "//*[@id='jobsearch']/div/div[2]/button"
 
-nextButton = "[aria-label='Next Page']"
-
 # Lists for extracted data for the spreadsheet
 job_title = []
 link_ = []
+location_ = []
+company_ = []
+
+# File name for the spreadsheet
+fileName = 'job_list.csv'
 
 # Takes in initial user input and searches it in Indeed
 def setupPage():
@@ -32,18 +33,22 @@ def setupPage():
 # Gets link text and hyperlinks and gets it ready for the spreadsheet
 def getJobs():
     links = driver.find_elements(By.CLASS_NAME, "jcs-JobTitle")
-    for link in links:
+    locations = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='text-location']")
+    companies = driver.find_elements(By.CSS_SELECTOR, "span[data-testid='company-name']")
+    for link, location, company in zip(links, locations, companies):
         job_title.append(link.text)
         link_.append(link.get_attribute("href"))
+        location_.append(location.text)
+        company_.append(company.text)
 
 # Puts extracted data into a spreadsheet and saves the file
 def createSpreadsheet():
-    df = pd.DataFrame({'Job Title': job_title, 'Link:': link_})
-    df.to_csv('job_list.csv', index=False)
+    df = pd.DataFrame({'Job Title': job_title, 'Company': company_, 'Location': location_, 'Link:': link_})
+    df.to_csv(fileName, index=False)
     print(df)
 
+# Code execution starts here
 setupPage()
 getJobs()
-#driver.find_element(By.CSS_SELECTOR, nextButton).click()
 driver.quit()
 createSpreadsheet()
