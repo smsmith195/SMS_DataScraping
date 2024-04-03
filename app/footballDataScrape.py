63f2078fecb38b8acb31089a6b92b4e1e1d14bd5
Stephen Mark Smith <smsmith195@gmail.com>
2024-04-03 16:00:43 +0100

    Update footballDataScrape.py and myAnimeListScrape.py

--- app/footballDataScrape.py
+++ app/footballDataScrape.py
@@ -3,78 +3,75 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 
+
 # Prompts user to select which country they wish to get the data from before launching Selenium
-countryChoice = input("Choose a Country, this is case-sensitive: ")
-print(countryChoice)
-leagueChoice = input("Choose a League. This is also case-sensitive: ")
-print(leagueChoice)
+def get_user_input():
+    country_choice = input("Choose a Country, this is case-sensitive: ")
+    league_choice = input("Choose a League. This is also case-sensitive: ")
+    return country_choice, league_choice
 
-# Opens Firefox on the Adam Choi website
-driver = webdriver.Firefox()
-driver.get("https://www.adamchoi.co.uk/overs/detailed")
-driver.implicitly_wait(5)
-
-# Create variables for page setup
-all_matches_button = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']")
-c_dropdown = Select(driver.find_element(By.ID, "country"))
-l_dropdown = Select(driver.find_element(By.ID, "league"))
-
-# XPATH references for table columns
-date_element = "./td[1]"
-home_element = "./td[2]"
-score_element = "./td[3]"
-away_element = "./td[4]"
-
-# Lists for containing the extracted data
-date = []
-home_team = []
-score = []
-away_team = []
-
-# Name of the file the data is being saved to
-fileName = "football_data.csv"
 
 # Set up page for extracting data, using country entered by the user. If the country is invalid, stop the application
-def setUpPage():
+def set_up_page(driver, country_choice, league_choice):
+    driver.get("https://www.adamchoi.co.uk/overs/detailed")
+    driver.implicitly_wait(5)
+    all_matches_button = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']")
     all_matches_button.click()
     try:
-        countryDropdownSelect()
-        leagueDropdownSelect()
+        country_dropdown_select(driver, country_choice)
+        league_dropdown_select(driver, league_choice)
     except:
-        closeOnFail()
+        close_on_fail(driver)
+
 
 # Selects country chosen by the user
-def countryDropdownSelect():
-    c_dropdown.select_by_visible_text(countryChoice)
+def country_dropdown_select(driver, country_choice):
+    c_dropdown = Select(driver.find_element(By.ID, "country"))
+    c_dropdown.select_by_visible_text(country_choice)
+
 
 # Selects league chosen by user
-def leagueDropdownSelect():
-    l_dropdown.select_by_visible_text(leagueChoice)
+def league_dropdown_select(driver, league_choice):
+    l_dropdown = Select(driver.find_element(By.ID, "league"))
+    l_dropdown.select_by_visible_text(league_choice)
+
 
 # Closes down application if selections are invalid
-def closeOnFail():
+def close_on_fail(driver):
     print("Invalid selection, closing program down")
     driver.quit()
     quit()
 
-# Gets the table on the page and extracts data
-def getMatchData():
-    matches = driver.find_elements(By.TAG_NAME, "tr")
 
+# Gets the table on the page and extracts data
+def get_match_data(driver):
+    matches = driver.find_elements(By.TAG_NAME, "tr")
+    match_data = []
     for match in matches:
-        date.append(match.find_element(By.XPATH, date_element).text)
-        home_team.append(match.find_element(By.XPATH, home_element).text)
-        score.append(match.find_element(By.XPATH, score_element).text)
-        away_team.append(match.find_element(By.XPATH, away_element).text)
+        date = match.find_element(By.XPATH, "./td[1]").text
+        home_team = match.find_element(By.XPATH, "./td[2]").text
+        score = match.find_element(By.XPATH, "./td[3]").text
+        away_team = match.find_element(By.XPATH, "./td[4]").text
+        match_data.append({'Date': date, 'Home Team': home_team, 'Score': score, 'Away Team': away_team})
+    return match_data
+
 
 # This function makes use of Pandas to write the extracted data to a csv file
-def createSpreadsheet():
-    df = pd.DataFrame({'Date': date, 'Home Team': home_team, 'Score': score, 'Away Team': away_team})
-    df.to_csv(fileName, index=False)
+def create_spreadsheet(match_data, file_name='football_data.csv'):
+    df = pd.DataFrame(match_data)
+    df.to_csv(file_name, index=False)
     print(df)
 
+
 # Code execution starts here
-setUpPage()
-getMatchData()
-driver.quit()
-createSpreadsheet()
+def main():
+    country_choice, league_choice = get_user_input()
+    driver = webdriver.Firefox()
+    set_up_page(driver, country_choice, league_choice)
+    match_data = get_match_data(driver)
+    driver.quit()
+    create_spreadsheet(match_data)
+
+
+if __name__ == "__main__":
+    main()
--- app/myAnimeListScrape.py
+++ app/myAnimeListScrape.py
@@ -3,41 +3,35 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-# Open Firefox and load 'Top Anime' page of MyAnimeList
-driver = webdriver.Firefox()
-driver.get("https://myanimelist.net/topanime.php")
-driver.implicitly_wait(5)
-
-# Where the data is being extracted from
-top_anime = driver.find_elements(By.CLASS_NAME, "ranking-list")
-
-# XPATH references for the data being extracted
-rank_element = "./td[1]"
-title_element = "./td[2]/div[1]/div[2]/h3[1]"
-score_element = "./td[3]"
-
-# Lists for containing the extracted data
-rank = []
-title = []
-score = []
-
-# Name of the file the data is being saved to
-fileName = "top_anime_data.csv"
-
-# getData adds the appropriate element from top_anime to the appropriate lists
-def getData():
+# Adds the appropriate element from top_anime to the appropriate lists
+def get_anime_data(driver):
+    top_anime = driver.find_elements(By.CLASS_NAME, "ranking-list")
+    anime_data = []
     for anime in top_anime:
-        rank.append(anime.find_element(By.XPATH, rank_element).text)
-        title.append(anime.find_element(By.XPATH, title_element).text)
-        score.append(anime.find_element(By.XPATH, score_element).text)
+        rank = anime.find_element(By.XPATH, "./td[1]").text
+        title = anime.find_element(By.XPATH, "./td[2]/div[1]/div[2]/h3[1]").text
+        score = anime.find_element(By.XPATH, "./td[3]").text
+        anime_data.append({'Rank': rank, 'Title': title, 'Score': score})
+    return anime_data
 
 # This function makes use of Pandas to write the extracted data to a csv file
-def createSpreadhseet():
-    df = pd.DataFrame({'Rank': rank, 'Title': title, 'Score': score})
-    df.to_csv(fileName, index=False)
+def create_spreadsheet(anime_data, file_name='top_anime_data.csv'):
+    df = pd.DataFrame(anime_data)
+    df.to_csv(file_name, index=False)
     print(df)
 
-# Code execution starts here
-getData()
-driver.quit()
-createSpreadhseet()
+# Code execution
+def main():
+    driver = webdriver.Firefox()
+    driver.get("https://myanimelist.net/topanime.php")
+    driver.implicitly_wait(5)
+
+    anime_data = get_anime_data(driver)
+
+    driver.quit()
+
+    create_spreadsheet(anime_data)
+
+
+if __name__ == "__main__":
+    main()
